c7f8816b21efa28b0881378d3dc10721668c0526
Jonathan <jonitoadriano@gmail.com>
2023-01-24 15:40:16 -0600

    new app.jsx code

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function Header() {}
+
+function ProductsNew() {}
+
+function ProductsIndex() {}
+
+function Footer() {}
+
+function Content() {
+  return (
+    <div>
+      <ProductsNew />
+      <ProductsIndex />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
